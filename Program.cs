39fb775385efcdd65eb419e0374a96768b794cdf
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-14 21:57:39 +0100

    DB connection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+
+using SuperHeroAPI_DotNet7PatrickGYT.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -16,6 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//Middleware
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
