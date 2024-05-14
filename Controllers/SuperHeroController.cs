a33cc2ac89d78b2cdc3f1873d30f56ee86e1427b
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-14 21:39:45 +0100

    Add project files.

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using SuperHeroAPI_DotNet7PatrickGYT.Controllers.Entities;
+
+namespace SuperHeroAPI_DotNet7PatrickGYT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllHeroes()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name= "Test",
+                    FirstName= "Test",
+                    LastName= "Test",
+                    Place= "Test"
+                }
+            };
+            return Ok(heroes);
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using SuperHeroAPI_DotNet7PatrickGYT.Controllers.Entities;
+
+using System.Data.Common;
+
+namespace SuperHeroAPI_DotNet7PatrickGYT.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
